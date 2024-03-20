322b1dadd96c4fc2c7eeb5c94a5aa0a086d380aa
hyeokjinkim <hjkim@storelink.io>
2024-03-20 21:29:09 +0900

    fix: update

--- python/decorators.py
+++ python/decorators.py
@@ -1,7 +1,9 @@
 import time
 from functools import wraps
 
-def retry_with_delay(retry_count=3, delay_seconds=1):
+def retry_with_delay(retry_count: int = 3,
+                     delay_seconds: float = 1,
+                     verbose: int = 2):
     def decorator(func):
         @wraps(func)
         def wrapper(*args, **kwargs):
@@ -10,7 +12,10 @@ def retry_with_delay(retry_count=3, delay_seconds=1):
                     # 함수 호출
                     return func(*args, **kwargs)
                 except Exception as e:
-                    print(f"Retry {attempt + 1}/{retry_count}, Error: {e}")
+                    if verbose==1:
+                        print(f"Retry {attempt + 1}/{retry_count}")
+                    elif verbose>1:
+                        print(f"Retry {attempt + 1}/{retry_count}, Error: {e}")
                     # 지정된 시간만큼 대기
                     time.sleep(delay_seconds)
             # 모든 재시도 실패 후 예외를 다시 발생
