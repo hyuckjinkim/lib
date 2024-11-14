5275719180be4aae5b3485c5eab01f44493a4b7d
hyuckjinkim <hyuckjin12@naver.com>
2024-11-15 04:58:47 +0900

    feat: get_device() 추가

--- python/torch/__init__.py
+++ python/torch/__init__.py
@@ -11,3 +11,6 @@ def seed_everything(seed):
     torch.cuda.manual_seed(seed)
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = True
+
+def get_device():
+    return torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
